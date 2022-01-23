e31a7cc66417d1b96f7ebbae69a0a3808fa8884f
GuilhermeTeixeira13 <guilherme.teixeira@ubi.pt>
2022-01-23 03:55:11 +0000

    Update UnusualMinesweeper.java

--- UnusualMinesweeper.java
+++ UnusualMinesweeper.java
@@ -20,6 +20,7 @@ public class UnusualMinesweeper {
 
             int n = Integer.parseInt(N_K_String[0]);
             int k = Integer.parseInt(N_K_String[1]);
+
             ArrayList<Mine> mines = new ArrayList<Mine>();
 
             for(int mineCount = 0; mineCount < n; mineCount++){
@@ -34,14 +35,22 @@ public class UnusualMinesweeper {
                 mines.add(mine);
             }
 
-            // tabuleiro recebe minas e k
+            Board board = new Board(k);
+            board.setMines(mines);
+
+            board.buildBoard();
         }
     }
 }
 
 class Mine {
-    Point location;
+    Point location = new Point();
     private int TimeUntilExplosion;
+
+    public Mine(){
+        Point location = new Point();
+        this.TimeUntilExplosion = TimeUntilExplosion;
+    }
 
     public Mine(int TimeUntilExplosion){
         Point location = new Point();
@@ -54,8 +63,87 @@ class Mine {
     public double getLocationY(){
         return this.location.getY();
     }
+    public int getTimeUntilExplosion(){
+        return TimeUntilExplosion;
+    }
 
-    public void setLocation(double x, double y){
-        this.location.setLocation(x, y);
+    public void setLocation(int x, int y){
+        this.location.x = x;
+        this.location.y = y;
+    }
+}
+
+class Board {
+    ArrayList<Mine> mines;
+    int k;
+
+    public Board(int k){
+        ArrayList<Mine> mines = new ArrayList<Mine>();
+        this.k = k;
+    }
+
+    public void setMines(ArrayList<Mine> mines){
+        this.mines = (ArrayList<Mine>) mines.clone();
+    }
+
+    public int calculateBoardSize(){
+        double lowerCordinateX = 0;
+        double lowerCordinateY = 0;
+        double higherCordinate = 0;
+        int transactionX = 0;
+        int transactionY = 0;
+        Mine mineFarAwayX = new Mine();
+        Mine mineFarAwayY = new Mine();
+        for(int i = 0; i < this.mines.size() ; i++){
+            if(mines.get(i).getLocationX() < lowerCordinateX){
+                mineFarAwayX = this.mines.get(i);
+                lowerCordinateX = mines.get(i).getLocationX();
+            }
+            if(mines.get(i).getLocationY() < lowerCordinateY){
+                mineFarAwayY = this.mines.get(i);
+                lowerCordinateY = mines.get(i).getLocationY();
+            }
+        }
+        transactionX = (int)mineFarAwayX.getLocationX()*-1;
+        transactionY = (int)mineFarAwayY.getLocationY()*-1;
+
+        for(int i = 0; i < this.mines.size() ; i++){
+            mines.get(i).setLocation((int)mines.get(i).getLocationX()+transactionX, (int)mines.get(i).getLocationY()+transactionY);
+
+            if(mines.get(i).getLocationX() > higherCordinate)
+                higherCordinate = mines.get(i).getLocationX();
+            if(mines.get(i).getLocationY() > higherCordinate)
+            higherCordinate = mines.get(i).getLocationY();
+        }
+
+        return (int)higherCordinate;
+    }
+
+    public void printBoard(int higherCordinate, int[][] board){
+        System.out.println();
+        for(int y=higherCordinate; y>=0; y--){
+            for(int x=0; x<=higherCordinate; x++){
+                System.out.print(board[y][x]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void buildBoard(){
+        int higherCordinate = calculateBoardSize();;
+
+        int[][] board = new int[higherCordinate+1][higherCordinate+1];
+
+        for(int y=0; y<higherCordinate; y++){
+            for(int x=0; x<higherCordinate; x++){
+                board[y][x] = 0;
+            }
+        }
+
+        for(int i = 0; i<this.mines.size(); i++){
+            board[(int)this.mines.get(i).getLocationY()][(int)this.mines.get(i).getLocationX()] = this.mines.get(i).getTimeUntilExplosion();
+        }
+
+        printBoard(higherCordinate, board);
     }
 }
