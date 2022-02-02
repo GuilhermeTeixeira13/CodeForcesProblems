2cca34c0e319b6c96e809b8e9741ccaaa3436797
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2022-02-02 12:53:49 +0000

    Update P1263D.java

--- P1263D.java
+++ P1263D.java
@@ -4,7 +4,6 @@ import java.util.*;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.io.InputStreamReader;
-import java.io.BufferedOutputStream;
 
 public class P1263D {
     public static FastReader sc;
@@ -74,10 +73,10 @@ public class P1263D {
         	palavras.add(in.nextLine());
     	}
 
-        
+        System.out.println(equivalentes(palavras.get(0), palavras));
     }
 
-    public Boolean equivalentesBoolean(String a, String b, ArrayList<String> arr){
+    public static Boolean equivalentesR1(String a, String b){
         boolean equi = false;
 
         for(int i=0; i<a.length();i++){
@@ -86,27 +85,38 @@ public class P1263D {
             }
         }
 
+        return equi;
+    }
+
+    public static Boolean equivalentesR2(String a, String b, ArrayList<String> arr){
+        boolean equi = false;
+ 
+        equi = equivalentesR1(a, b);
+
         int pos = 0;
         while(pos<arr.size()){
-            if(equivalentesBoolean(a, arr.get(pos), arr) == true || equivalentesBoolean(b, arr.get(pos), arr) ==  true)
+            if(equivalentesR1(a, arr.get(pos)) == true && equivalentesR1(b, arr.get(pos)) ==  true)
                 equi = true;
+            pos++;
         }
 
         return equi;
     }
 
-    public ArrayList<String> equivalentes(String a, ArrayList<String> arr){
+    public static ArrayList<String> equivalentes(String a, ArrayList<String> arr){
         ArrayList<String> equivalentesStringA = new ArrayList<String>();
 
         for(int i=0; i<arr.size(); i++){
             for(int j=0; j<a.length(); j++){
                 if(arr.get(i).indexOf(a.charAt(j)) != -1){
                     equivalentesStringA.add(arr.get(i));
+                    break;
                 }
             }
             for(int j=0; j<arr.size(); j++){
-                if((equivalentesBoolean(a, arr.get(j), arr) == true || equivalentesBoolean(arr.get(i), arr.get(j), arr) ==  true) && i != j){
+                if((equivalentesR2(a, arr.get(j), arr) == true && equivalentesR2(arr.get(i), arr.get(j), arr) ==  true) && i != j){
                     equivalentesStringA.add(arr.get(i));
+                    break;
                 }
             }
         }
